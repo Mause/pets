3387a3d6861127036b99a40ea69c0845554249aa
Dominic May <me@mause.me>
2018-09-30 22:39:24 +0800

    Add index.json route

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from itertools import chain
 from concurrent.futures import ThreadPoolExecutor as PoolExectutor
 
 from tqdm import tqdm
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 from sources import sources
 
@@ -12,8 +12,7 @@ EXECUTOR = PoolExectutor()
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
+def get_data():
     data = chain.from_iterable(
         EXECUTOR.map(
             lambda func: list(func()),
@@ -22,7 +21,16 @@ def index():
     )
     data = tqdm(data)
     data = sorted(data, key=lambda item: item.found_on, reverse=True)
-    return render_template('index.html', data=data)
+
+
+@app.route('/index.json')
+def index_json():
+    return jsonify(get_data())
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', data=get_data())
 
 
 if __name__ == '__main__':
