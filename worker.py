c4c40014d4f218b2e1f4644272b3eb6e3d296700
Dominic May <me@mause.me>
2018-10-03 09:36:49 +0800

    Correct method call

--- worker.py
+++ worker.py
@@ -13,7 +13,7 @@ logger.setLevel(logging.DEBUG)
 def update_data():
     start = time.time()
     logging.info("Starting data update")
-    redis.put("data", pickle.dumps(get_data()))
+    redis.set("data", pickle.dumps(get_data()))
     logging.info("Update took %s seconds", time.time() - start)
 
 
