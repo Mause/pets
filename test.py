d58eb35440eef180367bb5f1d155c0045ff9d77e
Dominic May <me@mause.me>
2018-10-21 18:17:51 +0800

    Simple email test

---
+++ test.py
@@ -0,0 +1,19 @@
+import responses
+from urllib.parse import parse_qsl
+from worker import alert_error, MESSAGES_URL
+
+
+@responses.activate
+def test_alert_email():
+    responses.add(method="POST", url=MESSAGES_URL)
+
+    try:
+        raise Exception("What the heck?")
+    except Exception as e:
+        error = e
+
+    alert_error("Busso", error)
+
+    js = dict(parse_qsl(responses.calls[0].request.body))
+
+    assert js["html"].strip() == open("tests/data/email.html").read().strip()
