0b6cf455aaa4a8ce610ef5a28c5dac910af626b0
Elliana <me@mause.me>
2020-06-01 16:18:18 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -4,11 +4,10 @@ import sentry_sdk
 from sentry_sdk.integrations.flask import FlaskIntegration
 from sentry_sdk.integrations.redis import RedisIntegration
 
-from main import app
+from main import create_app
 
 sentry_sdk.init(
-    dsn=os.environ['SENTRY_DSN'],
-    integrations=[FlaskIntegration(), RedisIntegration()]
+    dsn=os.environ['SENTRY_DSN'], integrations=[FlaskIntegration(), RedisIntegration()]
 )
 
-app,
+app = create_app()
